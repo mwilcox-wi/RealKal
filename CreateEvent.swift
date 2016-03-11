168e2cecd0ff690ab1a6331c7c4dcc39d3f1e02e
Matthew Wilcox <mwilcox@Matthews-Mac.local>
2016-03-10 18:18:57 -0800

    Creating Event
    Setting up create event view with date pickers

---
+++ CreateEvent.swift
@@ -0,0 +1,26 @@
+//
+//  CreateEvent.swift
+//  RealKal
+//
+//  Created by Matthew Wilcox on 3/10/16.
+//  Copyright © 2016 Matthew Wilcox. All rights reserved.
+//
+
+import UIKit
+
+class CreateEvent: UIViewController {
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
